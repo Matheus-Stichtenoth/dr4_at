07e3d3b8daeba281bda2b3125a594b6473ffc38f
Matheus_Stick <matheus.stichtenoth@al.infnet.edu.br>
2024-12-12 21:52:32 -0300

    4️⃣ - Letra A Feita

--- src/dataprep.py
+++ src/dataprep.py
@@ -29,6 +29,10 @@ api_to_parquet(url_api,'deputados','data')
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#############################
+#########ATIVIDADE 3#########
+#############################
+
 #Lendo os dados do parquet criado para gerar o gráfico
 df_deputados = pd.read_parquet('data/deputados.parquet')
 
@@ -53,4 +57,27 @@ output_path = 'data/distribuicao_deputados.png'
 plt.savefig(output_path, dpi=300, bbox_inches='tight')
 plt.close()
 
-output_path
+#############################
+#########ATIVIDADE 4#########
+#############################
+
+ids = df_deputados['id'].unique()
+
+df_deputados_despesas = pd.DataFrame()
+
+# Limitar o loop para os primeiros 4 IDs como teste
+for id in range(len(ids)):
+    print(f'⚜ {ids[id]} - Lendo dados')
+    url_deputado = f'https://dadosabertos.camara.leg.br/api/v2/deputados/{ids[id]}/despesas'
+    response = requests.get(url_deputado)
+    data = response.json()
+    data_fixed = data.get("dados", [])
+    df_deputado = pd.DataFrame(data_fixed)
+    df_deputado = df_deputado[['tipoDespesa', 'dataDocumento']]
+    df_deputado['dataDocumento'] = pd.to_datetime(df_deputado['dataDocumento']).dt.date
+    df_deputado['id'] = ids[id]
+    
+    # Atualizar o DataFrame acumulado
+    df_deputados_despesas = pd.concat([df_deputados_despesas, df_deputado], ignore_index=True)
+
+df_deputados_despesas.to_parquet('data/serie_despesas_diárias_deputados.parquet')
